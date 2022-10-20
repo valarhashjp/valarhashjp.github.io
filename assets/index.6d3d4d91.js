ae2e92a22b86babd56b3e91ea2382c9ed52023e6
xlfguang <xlfguang@users.noreply.github.com>
2022-10-20 09:21:29 +0000

    deploy: 5617e0baba8837879981127024e5d8d9e612edc3

---
+++ assets/index.6d3d4d91.js
@@ -0,0 +1,1 @@
+import{X as o,v as n,E as r,q as t,a5 as c}from"./index.cb183b76.js";const a={};function s(_,i){const e=c("router-view");return t(),n("main",null,[r(e)])}const m=o(a,[["render",s]]);export{m as default};
